a2035bbd0d8c6d69c8578443cd80423edb8f75fb
Mark <mark.nyambane@student.moringaschool.com>
2023-04-18 21:49:11 +0300

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import blogData from "../data/blog";
-
+import Header from "./Header";
+import About from "./About";
+import ArticleList from "./ArticleList";
 console.log(blogData);
 
 function App() {
@@ -8,6 +10,10 @@ function App() {
     <div className="App">
       You're on your own from here! Follow the deliverables; test things out in
       the browser as you write your code; and good luck!
+
+      <Header {...blogData} />
+      <About {...blogData} />
+      <ArticleList {...blogData} />
     </div>
   );
 }
